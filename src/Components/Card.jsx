b8518d530df6def21e1e518f7fc3be2e9e1d374e
Oskarnuz <oscar.nunez07@gmail.com>
2023-02-16 15:02:22 -0500

    Add the async function

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,13 +8,35 @@ export default function Card() {
 
     const addListMemberValue = (event) => {
         setListMember(event.target.value);
+        
     };
 
-    const addNewToList = (event) => {
+    async function  addNewToList(event) {
         event.preventDefault();
+        const data = {title: listMember}
+        console.log(data)
+        //llamar a la api para guardar el dato {title: “programar”}
+        const response = await fetch("http://localhost:8080/api/tasks", {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          });
+        console.log(response)
+
         setNewList([...newList, listMember]);
         setListMember('');
     };
+    const DeleteItems = (indexItem) => {
+        console.log("index", indexItem)
+        console.log("Estado actual", newList )
+        const result = newList.filter((item, index) => index !== indexItem )
+        console.log("Estado siguiente", result);
+        setNewList(result)
+      };
 
   return (
     <main className='container'>
@@ -31,11 +53,18 @@ export default function Card() {
             </form>
             <div className="list1">
                 <ul className='to-do-list'>
-                    {newList.map((item , index)=> <li>
+                    {newList.map((item , index)=> 
+                    <li key={index}>
                         <p> {index +1} - {item}</p>
-                        <button className="add">+</button>
-                        <button className="edit">E</button>
-                        <button className="delete">X</button>
+                        <button className="add" onClick={() => {
+                            alert("Tarea completada");
+                        }}>+</button>
+                        <button className="edit" onClick={() => {
+                            
+                        }}>E</button>
+                        <button className="delete" onClick={() => DeleteItems(index)}>
+
+                        </button>
                     </li>)}
                     
                 </ul>
